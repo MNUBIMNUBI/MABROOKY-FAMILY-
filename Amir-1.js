4ebe15967a677c92a5531ed8b4e46b627e03d809
SaidMbebed <saidmbebed22@gmail.com>
2025-09-11 22:57:29 +0300

    Add files via upload

---
+++ Amir-1.js
@@ -0,0 +1,29 @@
+
+
+        
+
+function mkh(){
+    
+    document.getElementById("xmn2").style.display="block";
+    setTimeout(function(){
+    document.getElementById("xmn2").style.display="none";
+},3000);
+}
+
+function JKM(){
+    let PK = document.getElementById("BUB").value;
+    let G = PK.length;
+
+    if(G<3 || !isNaN(PK) || PK==""){document.getElementById("HUB").innerHTML="Andika Jina sahihi";
+}else{let SSD = PK;
+      let ZZS = SSD;
+localStorage.setItem('ZZS',ZZS);
+ let GFR=localStorage.getItem('ZZS');
+
+
+document.getElementById("kiosk5").style.display="none";
+document.getElementById("GUESTx").innerHTML=GFR;
+}
+
+}
+
